4a4682baa1421c650e1855aaac318ef6d4906085
Murali_krishnan_Temenos <71455680+murali0051985@users.noreply.github.com>
2020-12-13 10:06:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome folks to docker via github!"
+    return "Welcome folks to docker via github2!"
 
 @app.route('/how are you')
 def hello():
